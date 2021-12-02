ded0585c0ae52562384b8c75082ff8e65354fcbf
rahulsrivastava30 <63499746+rahulsrivastava30@users.noreply.github.com>
2021-12-03 03:01:28 +0530

    mark 11

---
+++ script.js
@@ -0,0 +1,33 @@
+const dateOfBirth=document.querySelector("#date-of-birth");
+const luckyNumber=document.querySelector("#lucky-number");
+const checkNumberButton=document.querySelector("#check-number-btn");
+const outputBox=document.querySelector("#output-box");
+
+
+function checkBirthdayIsLucky(){
+    // console.log(luckyNumber.value=="");
+    const bdate=dateOfBirth.value;
+    const sum=calculateSum(bdate);
+    if(bdate&&luckyNumber.value!=""){
+        var message;
+        if(checkLuck(sum)) message="your birthday is lucky.";
+        else message="Your birthday is not lucky.";
+        outputBox.innerText=message;
+    }
+    else{
+        outputBox.innerText="pls enter the both fields.";
+    }
+}
+function checkLuck(sum){
+    if(sum%parseInt(luckyNumber.value)==0) return true;
+    return false;
+}
+function calculateSum(bdate){
+    bdate=bdate.replaceAll("-","");
+    let sum=0;
+    for(let index=0;index<bdate.length;index++){
+        sum+=parseInt(bdate.charAt(index));
+    }
+    return sum;
+}
+checkNumberButton.addEventListener("click",checkBirthdayIsLucky)
